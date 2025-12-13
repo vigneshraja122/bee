0da737019a032aa6c547a0b26a2d357c3418fed7
vigneshraja122 <vraja3868@gmail.com>
2025-12-13 16:25:12 +0530

    Initial push

---
+++ app/components/Hero.tsx
@@ -0,0 +1,47 @@
+import Topnav from "./Topnav";
+
+const Hero = () => {
+    return (
+        <div className="w-full h-screen flex flex-col items-center" style={{ backgroundImage: "url('/Desktop - 4.svg')" }}>
+            <Topnav />
+            <div className="w-full h-full flex flex-col justify-center items-center">
+                <img src="/Group 2.svg" />
+                <div className="w-[80%] h-full flex flex-col justify-center items-center text-center">
+                    <h1 className="text-4xl">World’s First AI-Powered Blockchain &<br/> Gaming Development Engine</h1>
+                    <p>Where Blockchain, Crypto, Metaverse, and AI-Automation come together to build the future</p>
+                    <div className="w-[70%] flex justify-between items-center"> 
+                        <div
+                        style={{
+                            backgroundImage: "url('/Group 305.svg')",
+                            backgroundRepeat: "no-repeat",
+                            backgroundPosition: "center",
+                            backgroundSize: "contain",
+                        }} className="w-40 h-20 flex justify-center items-center">
+                        <button
+                            className="flex items-center justify-center w-80 h-full text-white font-medium text-center mr-6"
+
+                        >
+                            Build Your Project
+                        </button>
+                    </div>
+                        <div
+                            style={{
+                                backgroundImage: "url('/Group 305.svg')",
+                                backgroundRepeat: "no-repeat",
+                                backgroundPosition: "center",
+                                backgroundSize: "contain",
+                            }} className="w-60 h-20 flex justify-center items-center">
+                            <button
+                                className="flex items-center justify-center w-full h-full text-white font-medium text-center mr-6"
+
+                            >
+                                Explore The Techverse
+                            </button>
+                        </div></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
