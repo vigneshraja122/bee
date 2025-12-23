e777a1a737c941b49ce37c34b244c86fdcab8ebd
srimathi1601 <ssrimathi1601@gmail.com>
2025-12-23 10:41:08 +0530

    Hero section responsive work updated for tab

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -11,14 +11,14 @@ const Hero = () => {
         }}>
             <Topnav />
             <div className="w-full h-full flex flex-col justify-center items-center">
-                <img src="/Group 2.svg" className="md:w-[350px] w-[250px]" />
+                <img src="/Group 2.svg" className="xl:w-[350px] lg:w-[350px] md:w-[390px] w-[250px]" />
                 <div className="w-[80%] h-full flex flex-col justify-center items-center text-center space-y-6 z-10">
                     <div className="space-y-4">
-                        <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-white md:w-[650px] w-full">The World’s First AI-Centered Blockchain Development Company</h1>
-                        <p className="text-md text-white">Where Blockchain, Crypto, Metaverse, and AI-Automation come together<br /> to build the future</p>
+                        <h1 className="text-2xl md:text-4xl lg:text-4xl font-semibold text-white md:w-[650px] w-full">The World’s First AI-Centered Blockchain Development Company</h1>
+                        <p className="text-md text-white">From custom blockchain development to AI-driven dApps, decentralized ecosystems,<br/> and digital asset infrastructures- we build what tomorrow demands.</p>
                     </div>
-                    <div className="md:w-[45%] w-full flex justify-between items-center">
-                        <GradientGlowButton className="">
+                    <div className="xl:w-[45%] lg:w-[45%] md:w-[100%]  w-full flex justify-around items-center">
+                        <GradientGlowButton className="md:text-sm">
                             Build Your Project
                         </GradientGlowButton>
                         <GradientGlowButton>
